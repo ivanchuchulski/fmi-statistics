442e9b3644e275fe1be8ec2c4aed693d0b02c4ca
Ivan Chuchulski <zake6@abv.bg>
2019-11-17 20:13:44 +0200

    fixes

--- project/projec-idea.r
+++ project/projec-idea.r
@@ -1,22 +1,26 @@
-# TODO make hist prettier
+# TODO 
+# 
+# 
 
 # from the package MASS we are using the dataset survey
 install.packages("MASS")
+library("MASS") # or require("MASS")
 
-require("survey")
+
+# 0. Анализ на една променлива
 
 
 # exercise, age, pulse
-
 # !!
 observationCount <- dim(survey)[1]
 no_na_survey <- subset(survey, !is.na(survey$Sex) & !is.na(survey$Wr.Hnd) & !is.na(survey$Pulse) & 
                                 !is.na(survey$Exer) & !is.na(survey$Smoke) & !is.na(survey$Height)) 
-
-
+ 
 # 1. Категорийни (обясняващи) VS категорийни (зависими)
 barplot(prop.table(x = table(no_na_survey$Exer, no_na_survey$Sex), margin = 2), legend.text = T)
+barplot(prop.table(x = table(survey$Sex, survey$Exer), margin = 2), legend.text = T)
 
+barplot(prop.table(x = table(survey$Exer, survey$Sex), margin = 2), legend.text = T)
 barplot(prop.table(x = table(survey$Sex, survey$Exer), margin = 2), legend.text = T)
 
 
@@ -24,6 +28,9 @@ barplot(prop.table(x = table(survey$Sex, survey$Exer), margin = 2), legend.text 
 #       sex         height or Wr.Hnd
 boxplot(no_na_survey$Height ~ no_na_survey$Sex)
 boxplot(no_na_survey$Wr.Hnd ~ no_na_survey$Sex)
+
+boxplot(survey$Height ~ survey$Sex)
+boxplot(survey$Wr.Hnd ~ survey$Sex)
 
 
 # have to remove na
