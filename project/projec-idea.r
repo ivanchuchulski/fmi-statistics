246c50bfb6be6f9f5746a6a91c3998c1da8b9c42
Ivan Chuchulski <zake6@abv.bg>
2019-11-23 18:42:43 +0200

    more ideas

--- project/projec-idea.r
+++ project/projec-idea.r
@@ -6,16 +6,36 @@
 install.packages("MASS")
 library("MASS") # or require("MASS")
 
+# all columns in dataframe survey
+# Sex, Wr.Hnd, NW.Hnd, W.Hnd, Fold, Pulse, Clap, Exer, Smoke, Height, M.I, Age
+
+# wanted columns
+# Sex, Wr.Hnd, Pulse, Exer, Smoke, Height, Age
+
+# getting the wanted columns and removing NA values
+columns_to_keep <- c("Sex", "Wr.Hnd", "Pulse", "Exer", "Smoke", "Height", "Age")
+
+survey_data <- survey[columns_to_keep]
+
+survey_data <- subset(survey_data, !is.na(survey_data$Sex) & 
+                                    !is.na(survey_data$Wr.Hnd) & 
+                                    !is.na(survey_data$Pulse) & 
+                                    !is.na(survey_data$Exer) & 
+                                    !is.na(survey_data$Smoke) & 
+                                    !is.na(survey_data$Height) & 
+                                    !is.na(survey_data$Age)) 
+# maybe round Age?
+
+# to write to csv file
+write.csv(survey_data, 'D:\\surveyData.csv', row.names = FALSE)
+
+# exercise, age, pulse
+survey_observations <- dim(survey)[1]
+survey_observations_ <- dim(survey_data)[1]
+
 
 # 0. Анализ на една променлива
 
-
-# exercise, age, pulse
-# !!
-observationCount <- dim(survey)[1]
-no_na_survey <- subset(survey, !is.na(survey$Sex) & !is.na(survey$Wr.Hnd) & !is.na(survey$Pulse) & 
-                                !is.na(survey$Exer) & !is.na(survey$Smoke) & !is.na(survey$Height)) 
- 
 # 1. Категорийни (обясняващи) VS категорийни (зависими)
 barplot(prop.table(x = table(no_na_survey$Exer, no_na_survey$Sex), margin = 2), legend.text = T)
 barplot(prop.table(x = table(survey$Sex, survey$Exer), margin = 2), legend.text = T)
@@ -25,13 +45,12 @@ barplot(prop.table(x = table(survey$Sex, survey$Exer), margin = 2), legend.text 
 
 
 # 2. Категорийни (обясняващи) VS числови (зависими)
-#       sex         height or Wr.Hnd
+#       sex to Height or Wr.Hnd
 boxplot(no_na_survey$Height ~ no_na_survey$Sex)
 boxplot(no_na_survey$Wr.Hnd ~ no_na_survey$Sex)
 
 boxplot(survey$Height ~ survey$Sex)
 boxplot(survey$Wr.Hnd ~ survey$Sex)
-
 
 # have to remove na
 sexes <- survey$Sex
