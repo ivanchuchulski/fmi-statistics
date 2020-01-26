1338bf72ebb7ff247644c589cec1e6a8e5884fba
Ivan Chuchulski <zake6@abv.bg>
2020-01-26 16:24:37 +0200

    additions

--- R-functions.r
+++ R-functions.r
@@ -3,30 +3,54 @@
 
 # location 
 	# mean(average, expectation)
-	meanFunction <- function(x) 
+	mymean <- function(x) 
 	{
-		sum(x) / length(x)
+		return (sum(x) / length(x))
 	}
 
 	# median
-	medianFunction <- function(x) 
-	{
+	mymedian <- function(x) {
 		sorted <- sort(x)
 		len <- length(x)
 
-		if (len %% 2 != 0)
-		{
-			return sorted[round(len / 2)]
+		if (len %% 2 == 0) {
+			return (mean(sorted[len/2 + c(0, 1)]))
 		}
-		else
-		{
-			return sorted[len / 2 + c(0, 1)]
+		else {
+			return (sorted[len / 2])
 		}
 	}
 
 	#mode
-	modeFunction <- function(x) 
-	{
+	mymode <- function(x) {
 		t <- table(x)
 		return (names(t)[t == max(t)])
 	}
+
+	mymode2 <- function(x) {
+		uniqx <- unique(x)
+		uniqx[which.max(tabulate(match(x, uniqx)))]
+	}
+
+	# variance
+	myvariation <- function(x) {
+		x_mean = sum(x) / length(x)
+    	sum((x - x_mean)^2) / (length(x) - 1)
+	}
+
+	# standard deviation
+	mysd <- function(x) {
+		sqrt(myvariation(x))
+	}
+
+	# mad (median absolute difference)
+	mymad <- function(x) {
+		x_median = mymedian(x)
+
+		mymedian(abs(x - x_median)) * 1.4826
+	}
+
+	myiqr <- function(x) {
+		quantile(x, 0.75) - quantile(x, 0.25)
+	}
+
